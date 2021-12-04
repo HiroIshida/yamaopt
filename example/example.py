2f6df4e64d22774f7d11d901fa0b88a74c8dc45f
HiroIshida <spitfire.docomo@gmail.com>
2021-12-05 00:31:51 +0900

    Example with vis

---
+++ example/example.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import time
+import skrobot
+import numpy as np
+from yamaopt.solver import KinematicSolver
+from yamaopt.polygon_constraint import polygon_to_trans_constraint
+
+if __name__=='__main__':
+    config_path = "../config/pr2_conf.yaml"
+    kinsol = KinematicSolver(config_path)
+
+    polygon = np.array([[0.5, -0.3, 0.0], [0.5, 0.3, 0.0], [0.0, 0.0, 0.6]])
+    polygon += np.array([0.3, -0.7, 0.7])
+
+    sol = kinsol.solve(-np.ones(7)*0.4, polygon, np.array([-2, 0, 0]))
+    kinsol.set_skrobot_angle_vector(sol.x)
+    assert sol.success
+
+    viewer = skrobot.viewers.TrimeshSceneViewer(resolution=(640, 480))
+    viewer.add(kinsol.robot)
+    viewer.show()
+
+    print('==> Press [q] to close window')
+    while not viewer.has_exit:
+        time.sleep(0.1)
+        viewer.redraw()
