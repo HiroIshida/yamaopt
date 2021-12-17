5c9282ff50c55b0d10f10e035b0938ac44f1f76c
Naoya Yamaguchi <708yamaguchi@gmail.com>
2021-12-17 19:14:41 +0900

    Update README.md and move visualizer.py (#12)
    * Update README.md
    
    * Move visualizer.py to yamaopt dir from example dir

--- example/example.py
+++ example/example.py
@@ -3,7 +3,7 @@ import argparse
 import numpy as np
 from yamaopt.solver import KinematicSolver, SolverConfig
 from yamaopt.polygon_constraint import polygon_to_trans_constraint
-from visualizer import VisManager
+from yamaopt.visualizer import VisManager
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
